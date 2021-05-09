ab08fcd8cd3e52a9443f37b7cf98358c9b1cac7e
Сергей <dervanovskij@gmail.com>
2021-05-09 20:11:25 +0700

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        boolean hw = bean==bean1;
+        boolean cat = cat1==cat2;
+        System.out.println(hw);
+        System.out.println(cat);
     }
 }
